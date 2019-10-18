2442c1bdb5c73c79de00454b2be594152054f76f
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-10-17 20:52:24 -0400

    Initial app files

---
+++ client/build/precache-manifest.202c7b351917e1bda207ce3c0626fd82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c8913bbef5051aa7e2a",
+    "url": "/static/css/main.92867234.chunk.css"
+  },
+  {
+    "revision": "4c8913bbef5051aa7e2a",
+    "url": "/static/js/main.3ac68336.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e0ecd5b1aef65dac07ed",
+    "url": "/static/js/2.3333d970.chunk.js"
+  },
+  {
+    "revision": "50835f2eb1e6826bf1d34f474a625b9d",
+    "url": "/index.html"
+  }
+];
